1fac8564b2e7f4d21370aa1ed1acf3a963c7c884
Sabin Thapa <thapsabin8@gmail.com>
2018-11-27 23:30:19 +0545

    home widgets and sections

--- wp-content/themes/computalaya/template-parts/post/content.php
+++ wp-content/themes/computalaya/template-parts/post/content.php
@@ -1,34 +1,68 @@
-<div class="transit-bouncein" data-delay="0">
+<?php 
+if (have_posts()):?>
 
-	<div class="blog-box padd-25">
+    <div id="blog" class="section">
 
-		<!-- <div class="blog-date marg-bott-25 wh-clr">12 Sep 2013</div> -->
-		<div class="blog-thumb" style="background-image: url('<?php the_post_thumbnail_url(); ?>');">
+        <div class="blog-content">
 
-			<div class="blog-caption transit">
-				<div class="blog-more nd-clr"><a href="<?php the_permalink(); ?>">Read More</a></div>
-				<div class="blog-icon"><i class="icon-paper"></i></div>
-			</div>
+            <div class="parallax" style="background-image: url('<?php echo get_bloginfo('template_directory'); ?>/maya/business/img/bg/what-we-do.png');">
 
-		</div>
+                <div class="blog-layer padd-y-25 bg-bk-alpha">
 
-		<div class="blog-detail padd-25 bg-gr1-clr">
+                    <div class="header-section padd-y-75">
 
-			<div class="blog-title fs-clr">
-				<a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
-			</div>
+                        <div class="title-section padd-x-25 wh-clr transit-words">What We Do</div>
 
-			<div class="line-center brd-gr2-clr"></div>
+                    </div>
 
-			<div class="blog-exc">
-				<?php the_excerpt(); ?>
-			</div>
+                    <div class="boxed">
 
-		</div>
+                        <div id="blog-carousel">
+                            <?php while (have_posts()) : $i = 1; the_post(); ?>
+                                
+                                <div class="transit-bouncein" data-delay="0">
 
-	</div>
+                                    <div class="blog-box padd-25">
 
-</div>
+                                        <!-- <div class="blog-date marg-bott-25 wh-clr">12 Sep 2013</div> -->
+                                        <div class="blog-thumb" style="background-image: url('<?php the_post_thumbnail_url(); ?>');">
 
+                                            <div class="blog-caption transit">
+                                                <div class="blog-more nd-clr"><a href="<?php the_permalink(); ?>">Read More</a></div>
+                                                <div class="blog-icon"><i class="icon-paper"></i></div>
+                                            </div>
 
+                                        </div>
 
+                                        <div class="blog-detail padd-25 bg-gr1-clr">
+
+                                            <div class="blog-title fs-clr">
+                                                <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+                                            </div>
+
+                                            <div class="line-center brd-gr2-clr"></div>
+
+                                            <div class="blog-exc">
+                                                <?php the_excerpt(); ?>
+                                            </div>
+
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            <?php endwhile; ?> 
+                        </div>
+
+                        <div class="clear"></div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div><!-- // BLOG -->
+<?php endif; ?>
